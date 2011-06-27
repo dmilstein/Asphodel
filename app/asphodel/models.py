27308e8ef7362f71c11df745191f4a6ce7503b08
Owen Raccuglia <ojracc@gmail.com>
2011-06-27 15:20:32 -0400

    Threw together a django app. Your move, Milstein.

---
+++ app/asphodel/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Game(models.Model):
+    name = models.CharField(max_length=200)
+    start_date = models.DateTimeField()
+    last_move_date = models.DateTimeField()
+
+class Player(models.Model):
+    game = models.ForeignKey(Game)
+    user = models.ForeignKey(models.User)
+    last_move_date = models.DateTimeField()
+    # score, etc.?
+
+class Planet(models.Model):
+    game = models.ForeignKey(Game)
+    name = models.CharField(max_length=40)
+    # more stats
+
